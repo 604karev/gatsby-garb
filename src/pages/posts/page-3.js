1d386eaa8a74edb396746e81a6b8ce51ffed11db
604karev <604karev@gmail.com>
2019-06-19 15:49:05 +0300

    init repo

---
+++ src/pages/posts/page-3.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../components/layout'
+import {Link} from "gatsby";
+
+export default () => (
+    <Layout>
+        <h1>Hello from gatsby</h1>
+        <Link to='/page-2'>Go to page-2</Link>
+    </Layout>
+
+
+)
+
